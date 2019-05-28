14524d24a693659f8e0f15056f0808294d8fb968
Abubakar Abid <a12d@stanford.edu>
2019-05-27 17:57:15 -0700

    fixed testfile absolute path issue

--- analyze.py
+++ analyze.py
@@ -6,7 +6,7 @@ def read_data(file_path, data_type):
     """
     reads data from file depending on type, to object that can be processed
     :param file_path: a string with the path to file that should be read
-    :param data_type: a string with the type of data in file (numerical, image, etc)
+    :param data_type: a string with the type of data in file (one of: [`numerical`])
     :return: a DataFrame with the loaded data (pandas df, etc)
     """
     if data_type == 'numerical':
@@ -24,7 +24,6 @@ def summarize_data(loaded_data):
     summarizes analysis of data (mean, median etc)
     :param loaded_data: a DataFrame which consists of the loaded data to be summarized
     :return: Pandas data description of DataFrame
-
     """
     return loaded_data.describe(include='all')
 
@@ -35,14 +34,13 @@ def compare_data(first_dataset_path, second_dataset_path, data_type, verbose=Fal
     :param first_dataset_path: a string with the path to the first file to compare (ex: representing training data)
     :param second_dataset_path: a string with the path to the second file to compare (ex: representing deployment data)
     :param data_type: a string with the type of data in the files to compare (ex: numerical)
+    :param verbose: a boolean that controls whether summary information should be printed.
     :return: a list with two items which are the data descriptions for the files
     """
     first_dataset_df = read_data(first_dataset_path, data_type)
     second_dataset_df = read_data(second_dataset_path, data_type)
-
     first_dataset_summary = summarize_data(first_dataset_df)
     second_dataset_summary = summarize_data(second_dataset_df)
-
     analysis = [first_dataset_summary, second_dataset_summary]
 
     if verbose:
@@ -50,7 +48,6 @@ def compare_data(first_dataset_path, second_dataset_path, data_type, verbose=Fal
         print(first_dataset_summary)
         print(f'A summary of data in {second_dataset_path} (your second file)..')
         print(second_dataset_summary)
-
     return analysis
 
 
--- test_analyze.py
+++ test_analyze.py
@@ -5,14 +5,14 @@ import os
 
 dir_path = os.path.dirname(os.path.abspath(__file__))
 
-test_train_filename = dir_path + '/test_data/train.csv'
-test_deploy_filename = dir_path + '/test_data/deploy.csv'
+TRAIN_TESTFILE = 'test_data/train.csv'
+DEPLOY_TESTFILE = 'test_data/deploy.csv'
 
 
 class AnalyzeTestCase(unittest.TestCase):
     def test_read_data(self):
-        self.assertIsInstance(analyze.read_data(test_train_filename, 'numerical'), pd.DataFrame)
-        self.assertIsInstance(analyze.read_data(test_deploy_filename, 'numerical'), pd.DataFrame)
+        self.assertIsInstance(analyze.read_data(TRAIN_TESTFILE, 'numerical'), pd.DataFrame)
+        self.assertIsInstance(analyze.read_data(DEPLOY_TESTFILE, 'numerical'), pd.DataFrame)
 
 
 if __name__ == '__main__':
